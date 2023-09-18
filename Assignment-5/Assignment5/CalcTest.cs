ed2fc5ef0c743d0e3378d758b44e28d919f5f2d1
KKaviya1 <Kaviyagandhi136@gmail.com>
2023-09-18 11:47:54 +0530

    my commit

---
+++ Assignment-5/Assignment5/CalcTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    [TestFixture]
+    public class CalcTest
+    {
+        [Test]
+        public void AddTest()
+        {
+            Calculator cal = new Calculator();
+            double num1 = 5;
+            double num2 = 5;
+
+            double result = num1 + num2;
+
+            Assert.AreEqual(result, cal.Add(5, 5));
+        }
+
+        [Test]
+        public void SubTractTest()
+        {
+            Calculator cal = new Calculator();
+            double num1 = 5;
+            double num2 = 5;
+
+            double result = num1 - num2;
+
+            Assert.AreEqual(result, cal.Sub(5, 5));
+        }
+
+        [Test]
+        public void DivideTest()
+        {
+            Calculator cal = new Calculator();
+            double num1 = 5;
+            double num2 = 5;
+
+            double result = num1 / num2;
+
+            Assert.AreEqual(result, cal.Div(5, 5));
+        }
+
+        [Test]
+        public void MultiplyTest()
+        {
+            Calculator cal = new Calculator();
+            double num1 = 5;
+            double num2 = 5;
+
+            double result = num1 * num2;
+
+            Assert.AreEqual(result, cal.Mul(5, 5));
+        }
+    }
+}
---
+++ Assignment-5/Assignment5/Program.cs
@@ -0,0 +1,25 @@
+﻿using Assignment5;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Calculator calc = new Calculator();
+        Console.WriteLine("Add two numbers");
+
+        Console.WriteLine("Result: " + calc.Add(12, 12));
+
+        Console.WriteLine("Subtract two numbers");
+
+        Console.WriteLine("Result: " + calc.Sub(12, 12));
+
+    
+        Console.WriteLine("Multiply two numbers");
+
+        Console.WriteLine("Result: " + calc.Mul(12, 12));
+
+        Console.WriteLine("Divide two numbers");
+
+        Console.WriteLine("Result: " + calc.Div(12, 12));
+    }
+}
